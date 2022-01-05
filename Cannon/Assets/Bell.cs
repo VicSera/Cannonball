6c971aec7000317d2e1390f9443da386e536cdcf
Victor Sera <Victor.Sera@cloudflight.io>
2022-01-05 23:37:43 +0200

    Add cannon, wall and bell with sound

---
+++ Cannon/Assets/Bell.cs
@@ -0,0 +1,18 @@
+﻿
+using System;
+using UnityEngine;
+
+public class Bell : MonoBehaviour
+{
+    private AudioSource _audioSource;
+    
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        _audioSource.Play();
+    }
+}
---
+++ Cannon/Assets/Cannon.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cannon : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody cannonball;
+    [SerializeField] private Transform front;
+    [SerializeField] private float forceMagnitude;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            var frontPos = front.position;
+            cannonball.transform.position = frontPos;
+            cannonball.velocity = (frontPos - transform.position).normalized * (forceMagnitude / cannonball.mass);
+        } 
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.down, 1f, Space.World);
+        } 
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(Vector3.up, 1f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.Rotate(Vector3.left, 1f, Space.World);
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.Rotate(Vector3.right, 1f, Space.World);
+        }
+    }
+}
